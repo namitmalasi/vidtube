3b3ccb9b56e722e5c4eb42675e748eab2ddff34e
Namit Malasi <namitmalasi@gmail.com>
2022-12-10 18:12:31 +0530

    configured database connectivity

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Database Connected!!!");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 app.listen(8800, () => {
+  connect();
   console.log("Server Running!!!");
 });
