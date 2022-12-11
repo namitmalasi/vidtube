dbea5909bc75fee62008683ac64734b373b325a6
Namit Malasi <namitmalasi@gmail.com>
2022-12-11 13:53:21 +0530

    created the folder structure for controllers and routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
-
 
 const connect = () => {
   mongoose
@@ -17,6 +20,11 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(8800, () => {
   connect();
